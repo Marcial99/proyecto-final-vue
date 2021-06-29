430451f8f9ad8729744ea4b845dd80814f15b829
17030752 <64864689+17030752@users.noreply.github.com>
2021-06-29 18:59:31 -0500

    modificaciones del dia 29/06/2021
    se instalaron los modulos de firebase para la solicitud de consulta personal con un medico.(todavia se desconoce donde se guardara)

--- src/components/User/Solicitud_Medico.vue
+++ src/components/User/Solicitud_Medico.vue
@@ -7,18 +7,61 @@
     <div id="emailHelp" class="form-text">Describa los sintomas aqui</div>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Evidencias (foto o video)</label>
-    <input type="file" class="form-control" id="exampleInputPassword1">
+    <label for="exampleInputPassword1" class="form-label">Evidencias (fotos)</label>
+    <input type="file" class="form-control" id="exampleInputPassword1" @change="onFileSelected" accept="image/jpeg,image/png,application/pdf">
   </div>
-  <button type="submit" class="btn btn-primary">Enviar</button>
-</form>
+  <div class="mb-3">
+    <progress :value="uploadValue" class="progress-bar progress-bar-striped" role="progressbar" style="width: 10%" 
+ v-bind:aria-valuenow="uploadValue" aria-valuemin="0" aria-valuemax="100" progressbar> % </progress>
+  </div>
+   <div class="container-fluid">
+<iframe :src="picture" style="width:500px; height:500px;" frameborder="0"></iframe>
+    </div>
+  <button type="submit" class="btn btn-primary" @click="onUpload">Enviar</button>
+  </form>
     </div>
 </template>
 
 <script>
-    export default {
-        name:'Solicitud_Medico'
+// @ is an alias to /src
+
+import firebase from 'firebase'
+export default {
+  name: 'Solicitud_Medico',
+  data() {
+    return {
+    selectedFile : null,
+    uploadValue:0,
+    picture:null,
     }
+  },
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+      console.log(this.selectedFile)
+    },
+    onUpload(){
+      var n = Date.now();
+      var storageRef = firebase.storage().ref(`/imagenes/${n}_${this.selectedFile.name}`);;
+      if(this.selectedFile.type=="application/pdf"){
+       storageRef = firebase.storage().ref(`/documentos/${n}_${this.selectedFile.name}`);
+      }
+
+const task = storageRef.put(this.selectedFile);
+task.on(`state_changed`,snapshot=>{
+  let percentage =(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+  this.uploadValue=percentage;
+},error=>{console.log(error.message)},
+  ()=>{this.uploadValue=100;
+  //downloadUrl
+  task.snapshot.ref.getDownloadURL().then((url)=>{
+    this.picture=url;
+    console.log(this.picture)
+  });
+  });
+}
+    }
+}
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -21,6 +21,17 @@ import {
 
 library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
 
+import firebase from 'firebase'
+
+  firebase.initializeApp({  
+    apiKey: "AIzaSyCr0ZCKaU6mfbfA4LKkmu78TPe5_5nFBZs",
+  authDomain: "tedw-2021.firebaseapp.com",
+  projectId: "tedw-2021",
+  storageBucket: "tedw-2021.appspot.com",
+  messagingSenderId: "377990299575",
+  appId: "1:377990299575:web:0f14fab1008315bddc7232"
+});
+
 Vue.config.productionTip = false
 Vue.use(VueToast);
 Vue.use(VeeValidate);
